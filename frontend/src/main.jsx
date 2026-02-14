12bce01e684d3a857ceb862665c6fd126956b96f
Ayushman <ayushmantripathi283@gmail.com>
2026-02-14 23:30:47 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,11 +9,11 @@ import SocketTest from "./test";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-        {/* <SocketTest /> */}
-      </Provider>
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+    {/* <Provider store={store}> */}
+    {/* <App /> */}
+    <SocketTest />
+    {/* </Provider> */}
+    {/* </BrowserRouter> */}
   </StrictMode>,
 );
