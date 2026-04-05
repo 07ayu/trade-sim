b4428cd65b7040d510220884c401ae1151ad8a79
Ayushman <ayushmantripathi283@gmail.com>
2026-04-05 22:11:04 +0530

    Cleanup root directory, unify .gitignore and consolidate backend dependencies

--- Server/routes/tradeRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const { buy } = require("../Controllers/tradeController")
-const { userVerification } = require("../middlewares/authMIddleware")
-
-router.post("/buy", userVerification, buy)
-
-module.exports = router
